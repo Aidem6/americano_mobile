86021dc58637cd3650ef0e5a4b001b15ff9daebb
Adam Tomczyk <atomczyk>
2024-03-02 10:45:43 +0100

    in-app purchases draft

--- App.jsx
+++ App.jsx
@@ -1,28 +1,83 @@
-import React from 'react';
+// import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './src/screens/Home';
-import Game from './src/screens/Game';
+// import Home from './src/screens/Home';
+// import Game from './src/screens/Game';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="Home"
+//           component={Home}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen
+//           name="Game"
+//           component={Game}
+//           options={{ headerShown: false }}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { withIAPContext } from "react-native-iap";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { Home } from "./src/screens/Home";
+import { Subscriptions } from "./src/screens/Subscriptions";
+
+export const screens = [
+  {
+    name: "Subscriptions",
+    title: "Subscriptions",
+    component: withIAPContext(Subscriptions),
+    section: "Context",
+    color: "#cebf38",
+  },
+  {
+    name: "Home",
+    component: Home,
+    section: "Context",
+    color: "#cebf38",
+  },
+];
+
+const Stack = createStackNavigator();
+
+export const StackNavigator = () => (
+  <Stack.Navigator screenOptions={{ title: "MainlyPaleo Subscriptions" }}>
+    {screens.map(({ name, component, title }) => (
+      <Stack.Screen
+        key={name}
+        name={name}
+        component={component}
+        //hide the header on these screens
+        options={{
+          title: title,
+          headerShown:
+            name === "Home" || name === "Subscriptions" ? false : true,
+        }}
+      />
+    ))}
+  </Stack.Navigator>
+);
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Game"
-          component={Game}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      <StackNavigator />
     </NavigationContainer>
   );
 }
